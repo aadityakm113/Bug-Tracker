94bd3112afa58e6ba282857d48057515987f28a8
aadityakm113 <aadityakm113@gmail.com>
2024-03-21 00:56:17 +0530

    store,reducer,actions

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import store from "./store";
+import { bugAdded } from "./actions";
+
+const unsubscribe=store.subscribe(() => {
+    console.log("Store changed!",store.getState());
+});
+
+store.dispatch(bugAdded("Bug 1"));
+
+unsubscribe();
+store.dispatch({
+    type:actions.BUG_REMOVED,
+    payload:{
+        id:1
+    }
+});
+console.log(store.getState());
