5e2b133b7648efb448dae2b72c271ecf18ffd483
aadityakm113 <aadityakm113@gmail.com>
2024-03-21 01:10:28 +0530

    bug-resolved

--- redux-starter/src/actions.js
+++ redux-starter/src/actions.js
@@ -3,9 +3,16 @@ import * as actions from './actionTypes';
 export const bugAdded=description=>({
     type:actions.BUG_ADDED,
         payload:{
-            description:"Bug1"
+            description
         }
 });
+
+export const bugResolved=id=>({
+    type:actions.BUG_RESOLVED,
+    payload:{
+        id
+    }
+})
 
 // export function bugAdded(description){
 //     return {
